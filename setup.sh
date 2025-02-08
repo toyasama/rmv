427ceee800288a3a9ad1a20010732ffcd8d7a29e
toya <emerytognama@gmail.com>
2025-02-08 09:41:44 +0000

    setup

---
+++ setup.sh
@@ -0,0 +1,3 @@
+alias rmv_api="docker exec -it --workdir=/home/devuser/workspace rmv_chore_dev_container bash"
+
+
---
+++ src/generate_uml.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIRECTORIES=(
+    "rmv_chore/rmv_chore/"
+    "rmv_chore/markers_management/"
+    "rmv_chore/parameters/"
+    "rmv_chore/tf_management/"
+    "rmv_chore/topic_management/"
+    "rmv_chore/utils/"
+    "rmv_chore/visualization/"
+)
+
+pyreverse -o puml  --all-associated  "${DIRECTORIES[@]}"
+
+if [ $? -eq 0 ]; then
+    echo "Diagramme UML généré avec succès !"
+else
+    echo " Erreur lors de la génération du diagramme UML."
+fi
